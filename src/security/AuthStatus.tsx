91529b60ad35b0e153bd09a8f792eddbe95bbb57
Hjalte Albergson <hjalte3480@gmail.com>
2024-03-01 18:04:09 +0100

    initial-commit

--- src/security/AuthStatus.tsx
+++ src/security/AuthStatus.tsx
@@ -1,5 +1,5 @@
 //import { useAuth } from "./_Authprovider";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function AuthStatus() {
   //const auth = useAuth();
